4b6c8169826a26fa3176a59430c5e554239860f6
Chris Eicher <eicherjc@gmail.com>
2021-10-19 11:28:27 -0400

    cleaned up some of the requests methods

--- BigPyD/DSAR.py
+++ BigPyD/DSAR.py
@@ -3,12 +3,14 @@ class DSAR:
     def __init__(self, session):
         self.s = session
 
-        r = session.get(url=session.url + "/api/v1/sar/profiles", headers=session.headers, verify=False)
+        r = session.get(url=session.url + "/api/v1/sar/profiles")
         self.profileId = r.json()['profiles'][0]['_id']
 
+
     def search(self, userName="", userId="", allAttributes="true"):
-        r = self.s.get(url=self.s.url + "/api/v1/sar/search/entity-sources?profileId=" + self.profileId + "&userName=" + userName + "&userId=" + userId + "&allAttributes=" + allAttributes, headers=self.s.headers, verify=False)
+        r = self.s.get(url=self.s.url + "/api/v1/sar/search/entity-sources?profileId=" + self.profileId + "&userName=" + userName + "&userId=" + userId + "&allAttributes=" + allAttributes)
         return r.json()
+
 
     def run(self, userId, displayName, attributes={}):
         data = {"profileId":"{}".format(self.profileId),
@@ -19,16 +21,30 @@ class DSAR:
             }
         }
 
-        r = self.s.post(url=self.s.url + "/api/v1/sar/reports", headers=self.s.headers, json=data)
+        r = self.s.post(url=self.s.url + "/api/v1/sar/reports", json=data)
         return r.json()
 
-    # def bulk(self):
+
+    def bulk(self, data):
+        r = self.s.post(url=self.s.url + "/api/v1/sar/reports/bulk", json=data)
+        return r.json()
+
+    def scans(self, limit="100", state="Completed"):
+        """The other option here for state would be "Started" """
+        r = self.s.get(url=self.s.url + "/api/v1/sar/scans?limit=" + limit + "&state=" + state)
+        return r.json()
+
+
+    def scanDetail(self, requestId):
+        r = self.s.get(url=self.s.url + "/api/v1/sar/scans/" + requestId)
+        return r.json()
 
 
     def delete(self, requestId):
-        r = self.s.post(url=self.s.url + "/api/v1/sar/reports/" + requestId + "/request-for-remove", headers=self.s.headers, verify=False)
+        r = self.s.post(url=self.s.url + "/api/v1/sar/reports/" + requestId + "/request-for-remove")
         return r.json()
 
+
     def profiles(self):
-        r = self.s.get(url=self.s.url + "/api/v1/sar/profiles", headers=self.s.headers, verify=False)
+        r = self.s.get(url=self.s.url + "/api/v1/sar/profiles")
         return r.json()
